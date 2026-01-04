34307f423555cb78f8ad0c21206f4fd6673589d8
SergioRossan <sergiokfc@gmail.com>
2026-01-04 18:31:20 +0000

    finaliza o endpoint de `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
+import { userAgentFromString } from "next/server";
 
+//função para retornar o status da página
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ mensagem: "uma mensagem para a Duda" });
+  //obtém a data dos dados apresentados na página convertida no formato ISO
+  const updateAt = new Date().toISOString();
+
+  //var para queries parametrizadas
+  const datName = process.env.POSTGRES_DB;
+  //const datName = request.query.datName;
+
+  //invoca database.js para consulta ao DB
+  const pgVersion = await database.query("SHOW server_version;");
+  var maxConnections = await database.query("SHOW max_connections;");
+  var usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [datName],
+  });
+
+  //converte para tipo "number"
+  maxConnections = Number(maxConnections.rows[0].max_connections);
+
+  //retornar o status da página e informações do DB
+  response.status(200).json({
+    update_at: updateAt,
+    database_info: {
+      pg_version: pgVersion.rows[0].server_version,
+      max_connections: maxConnections,
+      used_connections: usedConnections.rows[0].count,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET para /api/v1/status deveria retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedupdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedupdateAt);
+
+  expect(responseBody.database_info.pg_version).toBe("16.11");
+
+  expect(typeof responseBody.database_info.max_connections).toBe("number");
+
+  expect(typeof responseBody.database_info.used_connections).toBe("number");
 });
+
+/*test.only("teste SQL injection", async () => {
+  //await fetch("http://localhost:3000/api/v1/status?datName=local_db");
+  await fetch(
+    "http://localhost:3000/api/v1/status?datName='; SELECT pg_sleep(4); --",
+  );
+});*/
